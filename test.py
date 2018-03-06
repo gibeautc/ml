b082ca604e469125e83e1783bd26714c1ed302d7
ChadG <gibeautc@oregonstate.edu>
2018-03-06 15:48:38 -0800

    update

--- test.py
+++ test.py
@@ -1,75 +1,37 @@
 #!/usr/bin/env python
-
-
-class dataPoint:
-	def __init__(self):
-		self.temp=0
-		self.sky=0
-		self.dayPercent=0
-		self.maxPower=0
-		self.current=0
-		
+from sklearn.model_selection import train_test_split
+from sklearn import tree, metrics
+import matplotlib.pyplot as plt
+import numpy as np
 		
 X=[]
 Y=[]
 #   [temp,sky,dayPercent,Max Power]
-X.append([50,0,0,5])
-X.append([50,0,10,5])
-X.append([50,0,20,5])
-X.append([50,0,30,5])
-X.append([50,0,40,5])
-X.append([50,0,50,5])
-X.append([50,0,60,5])
-X.append([50,0,70,5])
-X.append([50,0,80,5])
-X.append([50,0,90,5])
-X.append([50,0,100,5])
+f=open("SolarData(4-6).csv")
+print(f.readline())
+data=f.readlines()
+for line in data:
+	l=line.split(",")
+	X.append([l[2],l[3],l[1],30])
+	Y.append(float(int(l[0])))
 
-# output current
-Y.append(20.0)
-Y.append(50.0)
-Y.append(70.0)
-Y.append(90.0)
-Y.append(100.0)
-Y.append(200.0)
-Y.append(100.0)
-Y.append(70.0)
-Y.append(50.0)
-Y.append(40.0)
-Y.append(10.0)
-
-
-X.append([50,0,50,5])
-X.append([50,10,50,5])
-X.append([50,20,50,5])
-X.append([50,30,50,5])
-X.append([50,40,50,5])
-X.append([50,50,50,5])
-X.append([50,60,50,5])
-X.append([50,70,50,5])
-X.append([50,80,50,5])
-X.append([50,90,50,5])
-X.append([50,100,50,5])
-
-Y.append(220.0)
-Y.append(200.0)
-Y.append(180.0)
-Y.append(160.0)
-Y.append(140.0)
-Y.append(120.0)
-Y.append(100.0)
-Y.append(80.0)
-Y.append(80.0)
-Y.append(70.0)
-Y.append(50.0)
+Xtrain,Xtest,Ytrain,Ytest=train_test_split(X,Y,test_size=0.33,random_state=42)
 
 
 
-
-from sklearn import tree
 #X = [[0, 0], [2, 2]]
 #y = [0.5, 2.5]
 clf = tree.DecisionTreeRegressor()
-clf = clf.fit(X, Y)
-print(clf.predict([[45,50,80,5]]))
-#array([ 0.5])
+clf = clf.fit(Xtrain, Ytrain)
+#print(clf.predict([[45,50,80,5]]))
+pred=[]
+for x in range(len(Xtest)):
+	pred.append(clf.predict([Xtest[x]]))
+score=metrics.mean_absolute_error(Ytest,clf.predict(Xtest))
+print("Mean ABS error: %f" % score)
+
+fig, ax = plt.subplots()
+ax.plot(pred)
+ax.plot(Ytest)
+
+plt.show()
